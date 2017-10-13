dc45a2c68de7ac9c01dbda3dc14833a4a91050e8
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-10-13 22:48:07 +0300

    Completed setting up Firebase integration

---
+++ FirebaseChat/Controller/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Stanly Shiyanovskiy on 13.10.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
